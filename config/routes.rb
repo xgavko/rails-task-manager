823f8231297432a04b74d9ed7d546d6ccdb5e35f
Xeniia Gavrilenko <X.gavrilenko@gmail.com>
2019-02-12 14:48:46 +0100

    implemented index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks' => 'tasks#index'
+  get 'tasks/new' => 'tasks#new'
+  get 'tasks/:id' => 'tasks#show', as: 'task'
+  post 'tasks' => 'tasks#create'
 end
